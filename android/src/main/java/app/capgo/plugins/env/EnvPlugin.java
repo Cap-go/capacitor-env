a5fda739763c0ef707b8311d03cfc509b53e665e
Martin Donadieu <martindonadieu@gmail.com>
2025-10-08 12:47:10 +0100

    fix: lint

--- android/src/main/java/app/capgo/plugins/env/EnvPlugin.java
+++ android/src/main/java/app/capgo/plugins/env/EnvPlugin.java
@@ -14,7 +14,7 @@ public class EnvPlugin extends Plugin {
         String key = call.getString("key");
 
         JSObject ret = new JSObject();
-        ret.put("value",  this.getConfig().getString("publicKey", ""));
+        ret.put("value", this.getConfig().getString("publicKey", ""));
         call.resolve(ret);
     }
 }
